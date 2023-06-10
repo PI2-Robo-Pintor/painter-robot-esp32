2dd3c61b385744f6fc33cc09f9e8eeee5a94d168
Yudi Yamane <yudiyamane@gmail.com>
2023-06-10 15:14:43 -0300

    feat: adiciona fila global pra sensores

--- main/queue.h
+++ main/queue.h
@@ -4,11 +4,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 
-QueueHandle_t qqueue;
+QueueHandle_t StepMotorQueue;
+
+QueueHandle_t SensorsQueue;
 
 #endif // QUEUE_H_INCLUDED
-
-/**
-     (mqtt.cpp.obj):/home/yudi/Documents/1-projetos/programming/unb/pi2-painter-robot-esp32/main/queue.h:7: multiple definition of `qqueue'; esp-idf/main/libmain.a(painter-robot-esp32.cpp.obj):/home/yudi/Documents/1-projetos/programming/unb/pi2-painter-robot-esp32/main/queue.h:7: first defined here
-(StepMotor.cpp.obj):/home/yudi/Documents/1-projetos/programming/unb/pi2-painter-robot-esp32/main/queue.h:7: multiple definition of `qqueue'; esp-idf/main/libmain.a(painter-robot-esp32.cpp.obj):/home/yudi/Documents/1-projetos/programming/unb/pi2-painter-robot-esp32/main/queue.h:7: first defined here
-*/
