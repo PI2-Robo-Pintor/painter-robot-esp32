d339e37830c16ca7e93d4083353d4d38c88a1503
gustavoduartemoreira <gustavoduartemoreira@gmail.com>
2023-06-15 06:09:17 -0300

    add rele.cpp rele.h

---
+++ main/rele.h
@@ -0,0 +1,15 @@
+#ifndef _RELE_H
+#define _RELE_H
+
+#include "driver/gpio.h"
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+
+
+
+
+
+
+#endif // _RELE_H
