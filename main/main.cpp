e6b88a537d6e9747d77342c04399e3852cf53251
Yudi Yamane <yudiyamane@gmail.com>
2023-06-30 14:56:01 -0300

    refactor: forward declaration do handler do sensor de fim de curso

--- main/main.cpp
+++ main/main.cpp
@@ -21,21 +21,23 @@
 #include "protocol_examples_common.h"
 
 #include "StepMotor.h"
-#include "data_command.h"
+#include "data_command_event.h"
 #include "mqtt.h"
 #include "queue.h"
 
 static const char* TAG              = "PI2-Robo-Pintor";
 static const char* tag_main_control = "Main loop control";
 
-static void IRAM_ATTR handle_end_stop(void* args) {
-    Command command = {
-        .type  = T_ON_OFF,
-        .value = 0,
-    };
+// A resposta dessa interrupção depende dos delays no loop principal de controle
+static void IRAM_ATTR handle_end_stop(void* args);
 
-    xQueueSendFromISR(mainQueue, &command, NULL);
-}
+void setup_end_stop_sensor() {
+    gpio_set_direction(PIN_END_STOP, GPIO_MODE_INPUT);
+    gpio_pulldown_en(PIN_END_STOP);
+    gpio_set_intr_type(PIN_END_STOP, GPIO_INTR_POSEDGE);
+    gpio_install_isr_service(0);
+    gpio_isr_handler_add(PIN_END_STOP, handle_end_stop, NULL);
+};
 
 extern "C" void app_main(void) {
     ESP_LOGI(TAG, "[APP] Free memory: %" PRIu32 " bytes", esp_get_free_heap_size());
@@ -59,18 +61,14 @@ extern "C" void app_main(void) {
      */
     ESP_ERROR_CHECK(example_connect());
 
-    // Coisas do sensor de fim de curso
-    gpio_set_direction(PIN_END_STOP, GPIO_MODE_INPUT);
-    gpio_pulldown_en(PIN_END_STOP);
-    gpio_set_intr_type(PIN_END_STOP, GPIO_INTR_POSEDGE);
-    gpio_install_isr_service(0);
-    gpio_isr_handler_add(PIN_END_STOP, handle_end_stop, NULL);
+    setup_end_stop_sensor();
 
     Mqtt mqtt;
     StepMotor motor;
 
+    // mainQueue      = xQueueCreate(10, sizeof(Command));
+    mainQueue      = xQueueCreate(10, sizeof(EventCommand));
     mqttQueue      = xQueueCreate(10, sizeof(AllData));
-    mainQueue      = xQueueCreate(10, sizeof(Command));
     stepMotorQueue = xQueueCreate(10, sizeof(Command));
     sensorsQueue   = xQueueCreate(10, sizeof(AllData));
     solenoidQueue  = xQueueCreate(10, sizeof(unsigned char));
@@ -100,8 +98,8 @@ extern "C" void app_main(void) {
     AllData data      = {
              .device = D_STEP_MOTOR,
              .relay  = {
-                  .on_off = 1,
-                  .id     = RID_COMPRESSOR,
+                  .value = 1,
+                  .id    = RID_COMPRESSOR,
         },
     };
 
@@ -111,50 +109,71 @@ extern "C" void app_main(void) {
             .type  = (Type)0,
             .value = 0,
         };
-        result = xQueueReceive(mainQueue, &command, 1);
+
+        EventCommand ec = {0};
+
+        // result = xQueueReceive(mainQueue, &command, 10);
+        result = xQueueReceive(mainQueue, &ec, 10);
         if (result != pdPASS) {
             // ESP_LOGE(tag, "Erro na fila?");
             // continue;
         }
 
-        switch (command.type) {
-        case T_NONE:
-            break;
-        case T_MAX_HEIGHT:
-            ESP_LOGW(tag_main_control, "Set MAX height");
-            break;
+        if (ec.type == T_EVENT) {
 
-        case T_MIN_HEIGHT:
-            ESP_LOGW(tag_main_control, "Set MIN height");
-            break;
+        } else {
+            switch (command.type) {
+            case T_NONE:
+                break;
+            case T_MAX_HEIGHT:
+                ESP_LOGW(tag_main_control, "Set MAX height");
+                break;
 
-        case T_ON_OFF:
-            if (command.value == ON) {
-                ESP_LOGI(tag_main_control, "Step motor start");
-                // if (motor.state != RUNNING)
-                motor.start();
-                // ligar compressor
+            case T_MIN_HEIGHT:
+                ESP_LOGW(tag_main_control, "Set MIN height");
+                break;
+
+            case T_ON_OFF:
+                if (command.value == ON) {
+                    ESP_LOGI(tag_main_control, "Step motor start");
+                    // if (motor.state != RUNNING)
+                    motor.start();
+                    // ligar compressor
+                }
+                // OFF
+                else {
+                    ESP_LOGI(tag_main_control, "Step motor stop");
+                    motor.stop();
+                    // desligar compressor
+                }
+                break;
+
+            case T_VELOCITY:
+                motor.set_speed(command.value);
+                break;
+
+            default:
+                ESP_LOGW(tag_main_control, "Command not recognized 0x%02X", command.type);
+                break;
             }
-            // OFF
-            else {
-                ESP_LOGI(tag_main_control, "Step motor stop");
-                motor.stop();
-                // desligar compressor
-            }
-            break;
-
-        case T_VELOCITY:
-            motor.set_speed(command.value);
-            break;
-
-        default:
-            ESP_LOGW(tag_main_control, "Command not recognized 0x%02X", command.type);
-            break;
         }
 
         AllData recv_data;
-        result = xQueueReceive(sensorsQueue, &recv_data, 1);
+        result = xQueueReceive(sensorsQueue, &recv_data, 10);
 
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(2 / portTICK_PERIOD_MS);
     }
+}
+
+void handle_end_stop(void* args) {
+    // Command command = {
+    //     .type  = T_ON_OFF,
+    //     .value = 0,
+    // };
+
+    EventCommand event;
+    event.type       = T_EVENT;
+    event.event.type = E_REACHED_LOWER_END_STOP_SENSOR;
+
+    xQueueSendFromISR(mainQueue, &event, NULL);
 }
