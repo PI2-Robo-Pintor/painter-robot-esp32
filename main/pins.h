3803af60ddfc88218446492296ba6d00004f84ad
Yudi Yamane <yudiyamane@gmail.com>
2023-07-05 15:23:01 -0300

    refactor: muda pino sensor fim de curso pra 26

--- main/pins.h
+++ main/pins.h
@@ -4,13 +4,13 @@
 #define PIN_SM_STEP      GPIO_NUM_19
 
 #define PIN_SM_LED    GPIO_NUM_3
-#define PIN_SM_ENABLE GPIO_NUM_22
+#define PIN_SM_ENABLE GPIO_NUM_21
 #define PIN_SM_PEND   GPIO_NUM_7
 #define PIN_SM_ALARM  GPIO_NUM_8
 
 // Sensor de fimde curso
-#define PIN_END_STOP GPIO_NUM_14
+#define PIN_END_STOP GPIO_NUM_26
 
 // Relé
-#define PIN_RELAY_1 GPIO_NUM_19
-#define PIN_RELAY_2 GPIO_NUM_22
+#define PIN_RELAY_VALVE      GPIO_NUM_19
+#define PIN_RELAY_COMPRESSOR GPIO_NUM_15
