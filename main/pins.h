a70ad8b68f2e385dfbf75a2cef0d7535a5cfd14f
Yudi Yamane <yudiyamane@gmail.com>
2023-07-06 18:56:38 -0300

    chore: define pinos de novo

--- main/pins.h
+++ main/pins.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define PIN_SM_DIRECTION GPIO_NUM_23
+#define PIN_SM_DIRECTION GPIO_NUM_22
 #define PIN_SM_STEP      GPIO_NUM_19
 
 // Não vamos usar
@@ -10,8 +10,14 @@
 #define PIN_SM_ALARM GPIO_NUM_8
 
 // Sensor de fim de curso
-#define PIN_END_STOP GPIO_NUM_32
+#define PIN_END_STOP GPIO_NUM_23
 
 // Relé
-#define PIN_RELAY_VALVE      GPIO_NUM_22
+#define PIN_RELAY_VALVE      GPIO_NUM_34
 #define PIN_RELAY_COMPRESSOR GPIO_NUM_2
+
+#define PIN_PRESSURE_SENSOR GPIO_NUM_32
+
+#define PIN_LED_RED    GPIO_NUM_26
+#define PIN_LED_GREEN  GPIO_NUM_33
+#define PIN_LED_YELLOW GPIO_NUM_25
