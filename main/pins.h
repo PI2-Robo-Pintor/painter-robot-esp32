4d692326ceba878f1366c52528460f21b1870ec0
Yudi Yamane <yudiyamane@gmail.com>
2023-07-06 09:15:51 -0300

    refactor: muda pinos relés e fim de curso

--- main/pins.h
+++ main/pins.h
@@ -3,14 +3,15 @@
 #define PIN_SM_DIRECTION GPIO_NUM_23
 #define PIN_SM_STEP      GPIO_NUM_19
 
-#define PIN_SM_LED    GPIO_NUM_3
-#define PIN_SM_ENABLE GPIO_NUM_21
-#define PIN_SM_PEND   GPIO_NUM_7
-#define PIN_SM_ALARM  GPIO_NUM_8
+// Não vamos usar
+// #define PIN_SM_ENABLE GPIO_NUM_21
+#define PIN_SM_LED   GPIO_NUM_3
+#define PIN_SM_PEND  GPIO_NUM_7
+#define PIN_SM_ALARM GPIO_NUM_8
 
-// Sensor de fimde curso
-#define PIN_END_STOP GPIO_NUM_26
+// Sensor de fim de curso
+#define PIN_END_STOP GPIO_NUM_32
 
 // Relé
-#define PIN_RELAY_VALVE      GPIO_NUM_19
-#define PIN_RELAY_COMPRESSOR GPIO_NUM_15
+#define PIN_RELAY_VALVE      GPIO_NUM_22
+#define PIN_RELAY_COMPRESSOR GPIO_NUM_2
